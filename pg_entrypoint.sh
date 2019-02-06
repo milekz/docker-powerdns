745d530984e418ba88d4eb98864b8c024924f013
Miłosz Szewczak <milek@milek.eu>
2019-02-06 06:38:21 +0100

    Update pg_entrypoint.sh

--- pg_entrypoint.sh
+++ pg_entrypoint.sh
@@ -9,7 +9,7 @@ set -e
 if $PGSQL_AUTOCONF ; then
   if [ -z "$PGSQL_PORT" ]; then
       PGSQL_PORT=5432
- 
+  fi
   # Set POSTGRESQL Credentials in pdns.conf
   sed -r -i "s/^[# ]*launch=gpgsq.*/launch=gpgsql/g" /etc/pdns/pdns.conf
   sed -r -i "s/^[# ]*gpgsql-host=.*/gpgsql-host=${PGSQL_HOST}/g" /etc/pdns/pdns.conf
@@ -17,7 +17,7 @@ if $PGSQL_AUTOCONF ; then
   sed -r -i "s/^[# ]*gpgsql-user=.*/gpgsql-user=${PGSQL_USER}/g" /etc/pdns/pdns.conf
   sed -r -i "s/^[# ]*gpgsql-password=.*/gpgsql-password=${PGSQL_PASS}/g" /etc/pdns/pdns.conf
   sed -r -i "s/^[# ]*gpgsql-dbname=.*/gpgsql-dbname=${PGSQL_DB}/g" /etc/pdns/pdns.conf
-  fi
+  
   #MYSQLCMD="mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} --port=${MYSQL_PORT} -r -N"
 
   # wait for Database come ready
